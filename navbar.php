3ee441512fc9d2cd29d6559a5141b420254f0d30
Mechanic <52928434+Korkoe27@users.noreply.github.com>
2023-02-23 16:52:29 +0000

    built sidebar for easy navigation

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+    />
+    <link rel="stylesheet" href="checkTodo.css">
+    <title>Check To-do</title>
+</head>
+<body>
+    <aside class="sidebar">
+        <!-- links -->
+        <ul class="links">
+          <li>
+            <a href="">CREATE TASK</a>
+          </li>
+          <li>
+            <a href="">UPCOMING</a>
+          </li>
+          <li>
+            <a href="">CATEGORIES</a>
+          </li>
+          <li>
+            <a href="">DONE</a>
+          </li>
+          <li>
+            <a href="">COLLABORATORS</a>
+          </li>
+          <li>
+            <a href="">SETTINGS</a>
+          </li>
+          <li>
+            <a href="">PROFILE</a>
+          </li>
+          <li>
+            <a href="">LOGOUT</a>
+          </li>
+        </ul>
+      </aside>
